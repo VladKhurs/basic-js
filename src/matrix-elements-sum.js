f6eaf6f255a7d2ac1123df3ad0bbc08e83a741fe
Vladislav Khurs <130601125+VladKhurs@users.noreply.github.com>
2023-12-22 19:03:29 +0200

    Update matrix-elements-sum.js

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -1,26 +1,70 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * Given matrix, a rectangular matrix of integers,
- * just add up all the values that don't appear below a "0".
+ * In the popular Minesweeper game you have a board with some mines and those cells
+ * that don't contain a mine have a number in it that indicates the total number of mines
+ * in the neighboring cells. Starting off with some arrangement of mines
+ * we want to create a Minesweeper game setup.
  *
  * @param {Array<Array>} matrix
- * @return {Number}
+ * @return {Array<Array>}
  *
  * @example
  * matrix = [
- *  [0, 1, 1, 2],
- *  [0, 5, 0, 0],
- *  [2, 0, 3, 3]
+ *  [true, false, false],
+ *  [false, true, false],
+ *  [false, false, false]
  * ]
  *
- * The result should be 9
+ * The result should be following:
+ * [
+ *  [1, 2, 1],
+ *  [2, 1, 1],
+ *  [1, 1, 1]
+ * ]
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let arr = Array(matrix.length).fill(0).map(e => Array(matrix[0].length).fill(0))
+  for(let i = 0; i < matrix.length; i++) {
+    for(let j = 0; j < matrix[i].length; j++) {
+      if(matrix[i][j] === true) {
+        if((i-1 > -1 && i-1 < matrix.length) && (j-1 > -1 && j-1 < matrix.length)) {
+          arr[i-1][j-1] += 1
+        }
+
+        if((i-1 > -1 && i-1 < matrix.length) && (j > -1 && j < matrix.length)) {
+          arr[i-1][j] += 1
+        }
+        
+        if((i-1 > -1 && i-1 < matrix.length) && (j+1 > -1 && j+1 < matrix.length)) {
+          arr[i-1][j+1] += 1
+        }
+
+        if((i+1 > -1 && i+1 < matrix.length) && (j-1 > -1 && j-1 < matrix.length)) {
+          arr[i+1][j-1] += 1
+        }
+        
+        if((i+1 > -1 && i+1 < matrix.length) && (j > -1 && j < matrix.length)) {
+          arr[i+1][j] += 1
+        }
+        
+        if((i+1 > -1 && i+1 < matrix.length) && (j+1 > -1 && j+1 < matrix.length)) {
+          arr[i+1][j+1] += 1
+        }
+
+        if((i > -1 && i < matrix.length) && (j-1 > -1 && j-1 < matrix.length)) {
+          arr[i][j-1] += 1
+        }
+
+        if((i > -1 && i < matrix.length) && (j+1 > -1 && j+1 < matrix.length)) {
+          arr[i][j+1] += 1
+        }
+      }
+    }
+  }
+  return arr
 }
 
 module.exports = {
-  getMatrixElementsSum
+  minesweeper
 };
