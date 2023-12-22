5d09219a64935534cf181a925d32aaeba0283aa4
Vladislav Khurs <130601125+VladKhurs@users.noreply.github.com>
2023-12-22 19:01:30 +0200

    Update dns-stats.js

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let unick = [];
+  let all = [];
+  for (let i = 0; i < domains.length; i += 1) {
+    let dI = domains[i].split('.').reverse();
+    console.log(dI)
+    let s = '';
+    for (let j = 0; j < dI.length; j += 1) {
+      dI[j] = '.' + dI[j];
+      s += dI[j];
+      if (unick.indexOf(s) === -1) {
+        unick.push(s);
+      }
+      all.push(s);
+    }
+  }
+  let n = Array(unick.length).fill(0);
+  for (let i = 0; i < all.length; i += 1) {
+    n[unick.indexOf(all[i])] += 1;
+  }
+  
+  let ans = {};
+  for (let i = 0; i < unick.length; i += 1) {
+      ans[unick[i]] = n[i];
+  }
+
+  return ans;
 }
 
 module.exports = {
