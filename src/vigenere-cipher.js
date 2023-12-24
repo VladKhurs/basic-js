6e7c144b9071154089eff86432badc164faf4666
Vladislav Khurs <130601125+VladKhurs@users.noreply.github.com>
2023-12-24 22:43:49 +0200

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -21,15 +21,86 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class VigenereCipheringMachine {
   encrypt() {
-    if(arguments[0] === undefined || arguments[i] === undefined) {
+    if(arguments[0] === undefined || arguments[1] === undefined) {
       throw new Error('Incorrect arguments!');
     }
-    console.log(arguments[0])
+
+    let a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let ans = ''
+    let c = 0
+    let arg0 = arguments[0].toUpperCase()
+    let arg1 = arguments[1].toUpperCase()
+    console.log(arg0, arg1)
+    for(let i = 0; i < arg0.length; i++) {
+      let q = -3
+      let w = -3
+      if(a.indexOf(arg0[i]) !== -1 ) {
+          q = a.indexOf(arg0[i])
+          w = a.indexOf(arg1[c])
+          console.log('qw  ', q,w)
+          if (q + w > a.length-1) {
+              console.log('!!!  ', q,w, q + w - a.length, a[q + w - a.length-1], c)
+            ans += a[q + w - a.length]
+          }
+          else {
+              console.log('+++  ', q,w, q + w, a[q + w], c)
+            ans += a[q + w]
+          }
+          if(c === arg1.length - 1) {
+            c = 0
+          }
+          else {
+            c++
+          }
+      }
+      
+      else {
+          ans += arg0[i]
+      }
+    }
+
+    return ans
   }
   decrypt() {
-    if(arguments[0] === undefined || arguments[i] === undefined) {
+    if(arguments[0] === undefined || arguments[1] === undefined) {
       throw new Error('Incorrect arguments!');
     }
+
+    let a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let ans = ''
+    let c = 0
+    let arg0 = arguments[0].toUpperCase()
+    let arg1 = arguments[1].toUpperCase()
+    console.log(arg0, arg1)
+    for(let i = 0; i < arg0.length; i++) {
+      let q = -3
+      let w = -3
+      if(a.indexOf(arg0[i]) !== -1 ) {
+          q = a.indexOf(arg0[i])
+          w = a.indexOf(arg1[c])
+          console.log('qw  ', q,w)
+          if (q - w < 0) {
+              console.log('!!!  ', q,w, q + w - a.length, a[q + w - a.length-1], c)
+            ans += a[q - w + a.length]
+          }
+          else {
+              console.log('+++  ', q,w, q + w, a[q + w], c)
+            ans += a[q - w]
+          }
+          if(c === arg1.length - 1) {
+            c = 0
+          }
+          else {
+            c++
+          }
+      }
+      
+      else {
+          ans += arg0[i]
+      }
+    }
+
+    return ans
   }
 }
 
